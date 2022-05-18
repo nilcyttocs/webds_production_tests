e3d734404847205fc5da84dbab6bbdbb591fd1f8
Scott Lin <scott.lin@tw.synaptics.com>
2022-05-17 22:26:31 -0700

    Move to "WebDS - Testing" category

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
     launcher.add({
       command,
       args: { isLauncher: true },
-      category: "WebDS - Exploration"
+      category: "WebDS - Testing"
     });
 
     let tracker = new WidgetTracker<MainAreaWidget>({
