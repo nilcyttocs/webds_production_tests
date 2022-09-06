496d8481e5d9e5d01b1eed5107f5d97593ef9a12
Scott Lin <scott.lin@tw.synaptics.com>
2022-09-05 22:05:44 -0700

    Refactor CSS outer layout

--- src/widget_container.tsx
+++ src/widget_container.tsx
@@ -247,35 +247,34 @@ const ProductionTestsContainer = (props: any): JSX.Element => {
   const webdsTheme = props.service.ui.getWebDSTheme();
 
   return (
-    <div className="jp-webds-widget-body">
+    <>
       <ThemeProvider theme={webdsTheme}>
-        {initialized ? (
-          displayPage()
-        ) : (
-          <>
-            {alert && (
-              <Alert
-                severity="error"
-                onClose={() => setAlert(false)}
-                sx={{ whiteSpace: "pre-wrap" }}
-              >
-                {alertMessage}
-              </Alert>
-            )}
-            <div
-              style={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)"
-              }}
+        <div className="jp-webds-widget-body">
+          {alert && (
+            <Alert
+              severity="error"
+              onClose={() => setAlert(false)}
+              sx={{ whiteSpace: "pre-wrap" }}
             >
-              <CircularProgress color="primary" />
-            </div>
-          </>
+              {alertMessage}
+            </Alert>
+          )}
+          {initialized && displayPage()}
+        </div>
+        {!initialized && (
+          <div
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)"
+            }}
+          >
+            <CircularProgress color="primary" />
+          </div>
         )}
       </ThemeProvider>
-    </div>
+    </>
   );
 };
 
